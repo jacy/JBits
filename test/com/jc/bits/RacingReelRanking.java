b757d83af998188ab6d1686e0faed2bf328cd77c
jacy <johnsoncr.hong@gmail.com>
2019-05-13 17:40:04 +0800

    group first then sort after so that it might be better for reading.

--- test/com/jc/bits/RacingReelRanking.java
+++ test/com/jc/bits/RacingReelRanking.java
@@ -1,13 +1,13 @@
 package com.jc.bits;
 
 import static java.util.Comparator.comparing;
+import static java.util.Comparator.reverseOrder;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class RacingReelRanking {
@@ -24,23 +24,22 @@ public class RacingReelRanking {
 		machines.add(new Machine(8, 1, 13));
 		machines.add(new Machine(9, 1, 13));
 		machines.add(new Machine(10, 1, 3));
+
 		ranking(machines);
 	}
 
 	public static List<MachineRank> ranking(List<Machine> machines) {
-		return machines.stream().sorted(Machine.WINLOSE_DESC_ID_ASC).collect(groupingBy(Machine::winlose, LinkedHashMap::new, toList())).entrySet().stream().collect(
-			ArrayList::new, 
-			(accumulated, element) -> {
-				int rank = getLast(accumulated).map(MachineRank::nextRank).orElse(1);
-				accumulated.addAll(element.getValue().stream().map(m -> {
-					return new MachineRank(rank, m, element.getValue().size());
-				}).peek(System.out::println).collect(toList()));
-			}, 
-			ArrayList::addAll
-		);
+		return machines.stream().collect(groupingBy(Machine::winlose)).entrySet().stream().sorted(Map.Entry.comparingByKey(reverseOrder())).collect(
+				ArrayList::new, 
+				(accumulated, element) -> {
+					int rank = lastElement(accumulated).map(MachineRank::nextRank).orElse(1);
+					accumulated.addAll(element.getValue().stream().sorted(comparing(Machine::getId)).map(m -> {
+						return new MachineRank(rank, m, element.getValue().size());
+					}).peek(System.out::println).collect(toList()));
+				}, ArrayList::addAll);
 	}
 
-	public static Optional<MachineRank> getLast(ArrayList<MachineRank> accumulated) {
+	public static Optional<MachineRank> lastElement(ArrayList<MachineRank> accumulated) {
 		return accumulated.stream().reduce((first, second) -> second);
 	}
 
@@ -70,7 +69,6 @@ public class RacingReelRanking {
 		private Integer id = 0;
 		private Integer startBalance;
 		private Integer endBalance;
-		public static final Comparator<Machine> WINLOSE_DESC_ID_ASC = comparing(Machine::winlose).reversed().thenComparing(Machine::getId);
 
 		public Machine(Integer id, Integer startBalance, Integer endBalance) {
 			this.id = id;
